3efc554519a1fdad78fe487650843d33db69f644
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-07 20:13:15 -0400

    Created WorldTime class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,48 +8,29 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
+  String time = 'loading';
 
-    // make the request
-    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London'));
-    Map data = jsonDecode(response.body);
-
-    // Get properties from data
-    String datetime = data["datetime"];
-
-    // Get Sign
-    String sign = data['utc_offset'].substring(0, 1);
-
-    String offset1 = '00';
-    String offset2 = '00';
-
-    if (sign == '+')
-      {
-        offset1 = data['utc_offset'].substring(1,3);
-        offset2 = data['utc_offset'].substring(4,6);
-      }
-    else
-      {
-        offset1 = data['utc_offset'].substring(0,3);
-        offset2 = data['utc_offset'].substring(4,6);
-      }
-
-    // Create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset1), minutes: int.parse(offset2)));
-
+  void setupWorldTime () async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,48 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; // location name for the UI
+  late String time; // the time of the location
+  String flag; // url to asset flag icon
+  String url; // location url for api endpoint
+
+  WorldTime({ required this.location, required this.flag, required this.url });
+
+  Future<void> getTime() async {
+
+    // make the request
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+
+    // Get properties from data
+    String datetime = data["datetime"];
+
+    // Get Sign
+    String sign = data['utc_offset'].substring(0, 1);
+
+    String offset1 = '00';
+    String offset2 = '00';
+
+    if (sign == '+')
+    {
+      offset1 = data['utc_offset'].substring(1,3);
+      offset2 = data['utc_offset'].substring(4,6);
+    }
+    else
+    {
+      offset1 = data['utc_offset'].substring(0,3);
+      offset2 = data['utc_offset'].substring(4,6);
+    }
+
+    // Create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset1), minutes: int.parse(offset2)));
+
+    // Set the time property
+    time = now.toString();
+
+  }
+
+}
